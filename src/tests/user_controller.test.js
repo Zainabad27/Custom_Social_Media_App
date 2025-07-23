42932f1ca802a83745099b3c17417222b4bee504
Zain <zainabad27@gmail.com>
2025-07-23 23:01:25 +0500

    add integration tests for user register and user login.

--- src/tests/user_controller.test.js
+++ src/tests/user_controller.test.js
@@ -1,9 +1,15 @@
 import request from "supertest";
 import { afterAll, beforeAll, it, expect, describe } from "vitest";
-import { user_controller } from "../DI_classes.js/user.class.js";
 import { app } from "../app.js";
 import { connect_testing_db } from "../database/testing_db.js";
+import mongoose from "mongoose";
 
+// setup to upload a file
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import dotenv from 'dotenv';
 dotenv.config({
@@ -15,22 +21,35 @@ beforeAll(async () => {
     console.log("Testing db connected.");
 });
 
-import mongoose from "mongoose";
+
 
 afterAll(async () => {
     await mongoose.connection.close();
 });
 
 describe("testing the user controller.", () => {
-    //   const test_obj=new user_controller(fakemodel);
+    const filepath = path.join(__dirname, "zain.jpg");
+    it("should register a user", async () => {
+        const res =await request(app)
+        .post("/api/v1/users/register/through/classes")
+        .attach("avatar",filepath)
+        .field("username","zain2")
+        .field("password","123456")
+        .field("email","abcd2@gmail.com")
+        .field("fullname","zainabad2")
+
+        expect(res.status).toBe(201);
+
+    
+    })
 
     it("should login successfully.", async () => {
         const res = await request(app).post("/api/v1/users/login/through/classes").send({
-            "username": "abcd",
+            "username": "zain",
             "password": "12345"
         })
 
-        expect(res.statuscode).toBe(201);
+        expect(res.status).toBe(201);
     })
 
 
